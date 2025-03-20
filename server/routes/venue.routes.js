83c384d0628659e91d39e3c3b7535f2cba408d5e
mjkj09 <makejo09@gmail.com>
2025-03-20 04:48:03 +0100

    refactor(routes): simplify venue routes comments

--- server/routes/venue.routes.js
+++ server/routes/venue.routes.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 const venueController = require('../controllers/venue.controller');
 
-router.get('/', venueController.getAllVenues);      // GET all venues
-router.get('/:id', venueController.getVenueById);   // GET single venue by ID
-router.post('/', venueController.createVenue);      // POST create new venue
-router.put('/:id', venueController.updateVenue);    // PUT update venue
-router.delete('/:id', venueController.deleteVenue); // DELETE venue
+// CRUD
+router.get('/', venueController.getAllVenues);
+router.get('/:id', venueController.getVenueById);
+router.post('/', venueController.createVenue);
+router.put('/:id', venueController.updateVenue);
+router.delete('/:id', venueController.deleteVenue);
 
 module.exports = router;
