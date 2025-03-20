b31ca807bbeed7186394115c1b15b09c7e49b097
mjkj09 <makejo09@gmail.com>
2025-03-20 04:46:07 +0100

    feat(routes): add authentication and user routes to the server

--- server/index.js
+++ server/index.js
@@ -14,5 +14,11 @@ mongoose.connect(process.env.MONGO_URI)
 const venueRoutes = require('./routes/venue.routes');
 app.use('/api/venues', venueRoutes);
 
+const authRoutes = require('./routes/auth.routes');
+app.use('/api/auth', authRoutes);
+
+const userRoutes = require('./routes/user.routes');
+app.use('/api/users', userRoutes);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
