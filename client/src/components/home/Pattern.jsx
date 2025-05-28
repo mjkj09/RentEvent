df34b28da303ef4b0b36c54a5f216c449d4fd9c0
mjkj09 <makejo09@gmail.com>
2025-05-28 19:29:52 +0200

    feat: add homepage background video and update asset references

---
+++ client/src/components/home/Pattern.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Pattern = () => (
+    <StyledWrapper>
+        <video
+            className="background"
+            src='../../../public/background/homePageBackground.mp4'
+            autoPlay
+            muted
+            loop
+            playsInline
+        />
+    </StyledWrapper>
+);
+
+const StyledWrapper = styled.div`
+    .background {
+        position: fixed;
+        inset: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        opacity: 0.75;
+        z-index: -10;
+        background: #FFFFFF;
+    }
+`;
+
+export default Pattern;
