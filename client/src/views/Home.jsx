4eb693c388b4ad97b9bcfb7ae58d317faf4dbcc1
mjkj09 <makejo09@gmail.com>
2025-05-31 20:12:02 +0200

    feat(home): add Home page layout with composed sections

---
+++ client/src/views/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Box } from '@mui/material';
+import NavBar from '../components/common/NavBar';
+import HeroSearch from '../components/home/HeroSearch';
+import CategoriesSection from '../components/home/CategoriesSection';
+import PopularVenues from '../components/home/PopularVenues';
+import Footer from '../components/common/Footer';
+
+export default function Home() {
+    return (
+        <Box sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+            <NavBar />
+            <Box sx={{ flex: 1 }}>
+                <HeroSearch />
+                <CategoriesSection />
+                <PopularVenues />
+            </Box>
+            <Footer />
+        </Box>
+    );
+}
